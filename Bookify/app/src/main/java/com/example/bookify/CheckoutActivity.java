9fd0976452dfc4cc33c3e37040abae5ec8ea06aa
SrikanthM08 <Srikanthreddym08@gmail.com>
2020-08-29 11:50:03 -0400

    checkout done

--- Bookify/app/src/main/java/com/example/bookify/CheckoutActivity.java
+++ Bookify/app/src/main/java/com/example/bookify/CheckoutActivity.java
@@ -1,5 +1,7 @@
 package com.example.bookify;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -7,16 +9,28 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CheckoutActivity extends AppCompatActivity {
     TextInputEditText name,number,dateExpiry,cvv;
     TextInputLayout nameLayout,numberLayout,dateLayout,cvvLayout;
     CheckBox checkBox;
     Button button;
-    String exDate;
     String namepattern = "[a-zA-Z]+";
 
 
@@ -38,6 +52,7 @@ public class CheckoutActivity extends AppCompatActivity {
 
         checkBox = findViewById(R.id.checkBoxSaveCard);
         button = findViewById(R.id.buttonPay);
+        Bundle bundle = getIntent().getExtras();
 
         name.addTextChangedListener(new TextWatcher() {
             @Override
@@ -140,23 +155,44 @@ public class CheckoutActivity extends AppCompatActivity {
                     cvvLayout.setError("Enter CVV Plz..");
                     cvvLayout.requestFocus();
                 } else {
+                    if(checkBox.isChecked()){
+                        //SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("hello,sign in",MODE_PRIVATE);
+                        //final String user = sharedPreferences.getString("email", "");
 
+                        final Map<String, Object> data = new HashMap<>();
+                        data.put("name", name.getText().toString().trim());
+                        data.put("cardnumber",number.getText().toString().trim());
+                        data.put("expiryDate",dateExpiry.getText().toString().trim() );
+                        data.put("CVV", cvv.getText().toString().trim());
+                        //data.put("email",user);
+
+                        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+                        db.collection("card").document().set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(CheckoutActivity.this, "Card Details Saved", Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+
+
+                    }
+                    Intent intent = new Intent(getApplicationContext(), Confirmation.class);
+                    startActivity(intent);
 
                 }
-                //Intent intent = new Intent(getApplicationContext(), ETicketActivity.class);
-                //intent.putExtra("owner", owner);
-               // startActivity(intent);
+
 
 
             }
 
-    });
+        });
     }
     @Override
     public void onStart() {
         super.onStart();
 
-                    }
+    }
 
 
 
